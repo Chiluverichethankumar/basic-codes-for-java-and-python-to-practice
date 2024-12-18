bacd6e6bae2c70f94d1116942addce1d5ca351d5
Chiluverichethankumar <chiluverichethankumar@gmail.com>
2024-12-19 00:20:06 +0530

    Added my project

---
+++ Javabasictoadv/add.java
@@ -0,0 +1,16 @@
+public class add {
+    static int f = 10;
+
+    public static void main(String args[]) {
+        int a = 10;
+        int b = 20;
+        b = 30;
+        int c = a + b;
+        System.out.println(c);
+        int d = b - a;
+        System.out.println(d);
+        System.out.println(f);
+
+    }
+
+}
---
+++ Javabasictoadv/swap.java
@@ -0,0 +1,14 @@
+public class swap {
+    public static void main(String args[]) {
+        int a, b, c;
+        a = 10;
+        b = 20;
+        c = 0;
+
+        c = a;
+        a = b;
+        b = c;
+        System.out.println(a + "\n" + b);
+    }
+
+}
