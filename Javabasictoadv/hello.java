340a9c4af8e7c87d0d55a5448e3fa08fb8bb1bca
ChiluverchethanKumar <chiluverichethankumar@gmail.com>
2025-10-31 21:01:00 +0530

    my coding practice

---
+++ Javabasictoadv/hello.java
@@ -0,0 +1,11 @@
+class hello {
+    public static void main(String args[]) {
+        System.out.println("Hello java");
+        String Str = "Chethan";
+        for(int i =0;i>Str.length();i++){
+            if
+
+        }
+
+    }
+}
---
+++ Javabasictoadv/swap.java
@@ -0,0 +1,20 @@
+public class swap {
+    public static void main(String args[]) {
+        int a, b, c;
+        a = 10;
+        b = 20;
+        c = 0;
+
+        c = a;
+        a = b;
+        b = c;
+        System.out.println(a + "\n" + b);
+
+        int A = 2;
+        int B = 10;
+        int power = (int) (Math.pow(B, A));
+        System.out.println(power);
+
+    }
+
+}
